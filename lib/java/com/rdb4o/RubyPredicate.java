d605b6ac6e26571a590addc9e2165daec52ca102
Tymon Tobolski <i@teamon.eu>
2009-04-09 21:40:15 +0200

    Cleanup

--- lib/java/com/rdb4o/RubyPredicate.java
+++ lib/java/com/rdb4o/RubyPredicate.java
@@ -2,12 +2,12 @@ package com.rdb4o;
 
 import com.db4o.query.*;
 
-public abstract class RubyPredicate extends com.db4o.query.Predicate<Rdb4oBase> {
-    public boolean match(Rdb4oBase obj) {
+public abstract class RubyPredicate extends com.db4o.query.Predicate<Rdb4oModel> {
+    public boolean match(Rdb4oModel obj) {
         //System.out.println("sprawdzam..");
         //return true;
         return rubyMatch(obj);
     }
 
-    public abstract boolean rubyMatch(Rdb4oBase obj);
+    public abstract boolean rubyMatch(Rdb4oModel obj);
 }
